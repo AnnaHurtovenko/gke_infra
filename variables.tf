f3ca44caca48b8745825316bd9ab3386ac26c507
AnnaHurtovenko <163589983+AnnaHurtovenko@users.noreply.github.com>
2024-05-05 12:34:47 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,15 @@
-variable "GOOGLE_REGION" {
-  type        = string
-  description = "Google cluster Region"
-}
-
 variable "GOOGLE_PROJECT" {
   type        = string
-  description = "Google work project"
+  description = "GCP project to use"
+}
+
+variable "GOOGLE_REGION" {
+  type        = string
+  default     = "us-central1-c"
+  description = "GCP region to use"
+}
+
+variable "GKE_NUM_NODES" {
+  type        = number
+  description = "Number of nodes"
 }
