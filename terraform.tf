3a7f3664ae0f920c213599fd739e8d97158e26bd
AnnaHurtovenko <163589983+AnnaHurtovenko@users.noreply.github.com>
2024-05-09 14:56:24 +0300

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,8 @@
-terraform {
-  required_providers {
-    kind = {
-      source = "justenwalker/kind"
-      version = "0.17.0"
-    }
-  }
-}
+#terraform {
+#  required_providers {
+#    kind = {
+#      source = "justenwalker/kind"
+#      version = "0.17.0"
+#    }
+#  }
+#}
